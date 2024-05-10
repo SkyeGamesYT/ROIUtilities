5579b662812339b87830c885258589657915c186
SkyeGamesYT <113136719+SkyeGamesYT@users.noreply.github.com>
2024-05-10 11:59:44 -0500

    Update maincog.py

--- static/cogs/maincog.py
+++ static/cogs/maincog.py
@@ -8,10 +8,11 @@ class MainCog:
 
   @commands.command(pass_context=True)
   async def slap(ctx, member:discord.User):
-  await ctx.send(f"{ctx.message.author.mention} slaps {member.mention}!") 
-
-
-
+    await ctx.send(f"{ctx.message.author.mention} slaps {member.mention}!") 
+  async def echo(ctx, *,args):
+    if ctx.author.id >= 0:
+        await ctx.send(args)
+      
 
   def setup(bot):
     bot.add_cog(MainCog(bot))
